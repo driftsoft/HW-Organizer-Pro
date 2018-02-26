f150341195e6343a2f2e02d104bcd9e416e50252
Geekcoder127 <23584729+Geekcoder127@users.noreply.github.com>
2018-02-26 21:28:31 +0000

    Added Toggle Button JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
+// Sign Out Functionality
 $("div.top button").on("click",function(){
 	firebase.auth().signOut();
+});
+
+// Toggle Views, Sorting, and Showing All/Incomplete Tasks
+$(".container div.toolbar button.show").on("click",function(){
+	var showText = ["Show All","Show All Incomplete"];
+	$(this).text(showText[(showText.indexOf($(this).text())+1)%2]);
+});
+$(".container div.toolbar button.sort").on("click",function(){
+	var sortByText = ["Sort By Date","Sort By Subject"];
+	$(this).text(sortByText[(sortByText.indexOf($(this).text())+1)%2]);
+});
+$(".container div.toolbar button.view").on("click",function(){
+	$(this).toggleClass("blockView");
 });
